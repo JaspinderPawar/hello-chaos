24cdece80916953f0ef66a9c47a4ed3710cc7291
jaspinder <jaspinder@ucreate.co.in>
2018-11-23 20:32:19 +0530

    added express

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-var http = require('http');
-//loads http module
-var app=http.createServer(function (req, res) {
-//creates server
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  //sets the right header and status code
-  res.end('Hello Chaos\n');
-  //outputs string with line end symbol
-}).listen(1337, "127.0.0.1");
-//sets port and IP address of the server
-//console.log('Server running at http://127.0.0.1:1337/');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
