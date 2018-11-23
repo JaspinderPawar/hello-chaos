6245073da043db10e35c168f5585d5edfa68bf06
jaspinder <jaspinder@ucreate.co.in>
2018-11-23 18:33:06 +0530

    Changed title

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code
-  res.end('Hello World\n');
+  res.end('Hello Chaos\n');
   //outputs string with line end symbol
 }).listen(1337, "127.0.0.1");
 //sets port and IP address of the server
-console.log('Server running at http://127.0.0.1:1337/');
+//console.log('Server running at http://127.0.0.1:1337/');
